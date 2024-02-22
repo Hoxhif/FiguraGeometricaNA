fa8b9c5973ca09de4a755b01c01f11be7bb5c36c
José Antonio Guirado González <trizador@gmail.com>
2024-02-22 17:29:08 +0100

    REFACTORIZACION DE NOMBRES

--- prueba/src/main/java/Prueba.java
+++ prueba/src/main/java/Prueba.java
@@ -11,21 +11,21 @@ public class Prueba {
         /* Queremos averiguar si la figura introducida es un cuadrado o un rectángulo
          * y mostrar su perímetro.
          */
-        double b, a;
-        String f;
+        double base, altura;
+        String forma;
         teclado = new Scanner(System.in);
         System.out.print("Introduce la base de la figura: ");
-        b = teclado.nextDouble();
+        base = teclado.nextDouble();
         System.out.print("Introduce la altura de la figura: ");
-        a = teclado.nextFloat();
-        if (b == a) {
-            f = "cuadrado";
+        altura = teclado.nextFloat();
+        if (base == altura) {
+            forma = "cuadrado";
         }else
             {
-                f = "rectángulo";
+                forma = "rectángulo";
             }
-            double perimetro = 2 * b + 2 * a;
-            System.out.println("La figura es un " + f + " y su perímetro es: " + perimetro);
+            double perimetro = 2 * base + 2 * altura;
+            System.out.println("La figura es un " + forma + " y su perímetro es: " + perimetro);
             teclado.close();
         }
     }
