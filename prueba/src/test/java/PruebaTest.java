d424637f7e9d227b75bbd1dfe91b5ee1d471db72
José Antonio Guirado González <trizador@gmail.com>
2024-02-22 17:57:58 +0100

    TEST UNITARIOS AÑADIDOS

---
+++ prueba/src/test/java/PruebaTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PruebaTest {
+    int a=2;
+    int b=2;
+
+
+@Test
+    public void testNombreEsCuadrado(){
+    if (a==b)
+        assertEquals(true,"cuadrado");
+}
+
+@Test
+    public void testNombreEsRectangulo(){
+        if (a!=b)
+    assertEquals(true,"rectangulo");
+}
+}
