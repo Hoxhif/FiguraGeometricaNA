69bac8de9a1cc9978c6ae713f8c63a9fcf9274a4
José Antonio Guirado González <trizador@gmail.com>
2024-02-22 17:33:11 +0100

    EXTRACCION DEL METODO

--- prueba/src/main/java/Prueba.java
+++ prueba/src/main/java/Prueba.java
@@ -18,14 +18,24 @@ public class Prueba {
         base = teclado.nextDouble();
         System.out.print("Introduce la altura de la figura: ");
         altura = teclado.nextFloat();
+        forma = tipoFigura(base, altura);
+        calculoPerimetro(base, altura, forma);
+    }
+
+    private static void calculoPerimetro(double base, double altura, String forma) {
+        double perimetro = 2 * base + 2 * altura;
+        System.out.println("La figura es un " + forma + " y su perímetro es: " + perimetro);
+        teclado.close();
+    }
+
+    private static String tipoFigura(double base, double altura) {
+        String forma;
         if (base == altura) {
             forma = "cuadrado";
         }else
             {
                 forma = "rectángulo";
             }
-            double perimetro = 2 * base + 2 * altura;
-            System.out.println("La figura es un " + forma + " y su perímetro es: " + perimetro);
-            teclado.close();
-        }
+        return forma;
     }
+}
